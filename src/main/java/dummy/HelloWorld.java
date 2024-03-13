e8e9d692934a2b1897ac0ae3ebccee8e8dfa1734
Satheesh47 <36806307+Satheesh47@users.noreply.github.com>
2024-03-13 16:24:58 +0530

    Added Comment in HelloWorld.java

--- src/main/java/dummy/HelloWorld.java
+++ src/main/java/dummy/HelloWorld.java
@@ -3,6 +3,7 @@ package dummy;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		// Dummy comment
 		System.out.println("Hello World!!");
 		System.out.println("I am Satheesh");
 		System.out.println("Update from Github");
