8eafc3327a3b4035e7a6c1c88f21b836ba559d53
Satheesh47 <satheesh.it47@gmail.com>
2024-03-14 09:58:48 +0530

    Committing from Eclipse

--- src/main/java/dummy/HelloWorld.java
+++ src/main/java/dummy/HelloWorld.java
@@ -3,10 +3,8 @@ package dummy;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// Dummy comment
 		System.out.println("Hello World!!");
 		System.out.println("I am Satheesh");
-		System.out.println("Update from Github");
 		// TODO Auto-generated method stub
 
 	}
